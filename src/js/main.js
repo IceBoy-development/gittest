9a2c02ff40ae536a602c1864bedf37b402dfeb8e
Ruslan Yuseev <yuseeva.n.83@gmail.com>
2020-07-29 19:59:56 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,70 @@
+"use strict";
+//типы данных
+/*let number = 4.6;
+console.log(number);//number
+
+console.log(4/0);//infinity
+
+
+console.log('ii'* 9);//nan
+
+
+const pers = 'dde';//строковый
+
+const bool = false;//логоический
+
+
+console.log(some);//null
+
+let und;
+console.log(und);//undefined
+
+const obj = { 
+    name: 'john',// объекты
+};
+console.log(obj.name);
+
+let rrt = ['wwww', 45, false];//массивы
+console.log(rrt[2]);*/
+//--------------------------------------------------------------------
+//alert, confirm,prompt
+
+//alert("hellow");//alert
+
+//const result = confirm('are you here?');
+//console.log(result);
+
+//const an = prompt('вам есть 18', '');
+//console.log(typeof(an));
+
+//const an = +prompt('вам есть 18', '');
+//console.log(typeof(an));
+
+//подтверждение частного случая объекта
+/*const peton = [];
+peton[0] = prompt('как твоё имя','');
+peton[1] = prompt('как твоё имя','');
+//document.write(peton);*/
+
+//интерполяция
+
+/*const user = 'Ivan';
+alert(`привет, ${user}`);*/
+console.log(4+ +'9');
+
+let incr = 10,
+decr = 10;
+console.log(++decr);
+
+
+console.log(10%5);
+
+
+let check = true,
+    close = true;
+    console.log(check||close);
+    
+
+
+
+
